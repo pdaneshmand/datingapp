831d54c3bcc0b95f110384a45e9567ca6e8a94c8
Pourdad Daneshmand <pourdad.daneshmand@gmail.com>
2020-01-10 00:06:44 +0330

    add login and register to angular project

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -21,6 +21,7 @@ namespace DatingApp.API.Controllers
         }
         // GET api/values
         [HttpGet]
+        [AllowAnonymous]
         public async Task<IActionResult> GetValues()
         {
             var values = await _contetx.Values.ToListAsync();
