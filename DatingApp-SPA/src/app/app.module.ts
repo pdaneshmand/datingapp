a935f17f636099120d245e4e9add60888e584db5
Pourdad Daneshmand <pourdad.daneshmand@gmail.com>
2020-01-31 16:02:04 +0330

    adding jwt token and boystrap themes

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
+import { BsDropdownModule } from 'ngx-bootstrap';
+
 import { AppComponent } from "./app.component";
 import { NavComponent } from "./nav/nav.component";
 import { AuthService } from "./_services/auth.service";
@@ -11,7 +13,12 @@ import { ErrorInterceptorProvider } from "./_services/error.interceptor";
 
 @NgModule({
   declarations: [AppComponent, NavComponent, HomeComponent, RegisterComponent],
-  imports: [BrowserModule, HttpClientModule, FormsModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    BsDropdownModule.forRoot()
+  ],
   providers: [ErrorInterceptorProvider, AuthService],
   bootstrap: [AppComponent]
 })
