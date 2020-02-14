cc58744e63b85927cec19ed14d0623d76791bb69
Pourdad Daneshmand <pourdad.daneshmand@gmail.com>
2020-02-14 18:44:15 +0330

    view and edit Profile

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -25,6 +25,9 @@ import { MemberCardComponent } from "./members/member-list/member-card/member-ca
 import { MemberDetailComponent } from "./members/member-list/member-detail/member-detail.component";
 import { MemberDetailResolver } from "./_resolvers/member-detail.resolver";
 import { MemberListResolver } from "./_resolvers/member-list.resolver copy";
+import { MemberEditComponent } from "./members/member-list/member-edit/member-edit.component";
+import { MemberEditResolver } from "./_resolvers/member-edit.resolver";
+import { PreventUnsavedChanges } from "./_guards/prevent-unsaved-changes-guard";
 
 export function tokenGetter() {
   return localStorage.getItem("token");
@@ -47,7 +50,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
     ListsComponent,
     MessagesComponent,
     MemberCardComponent,
-    MemberDetailComponent
+    MemberDetailComponent,
+    MemberEditComponent
   ],
   imports: [
     BrowserModule,
@@ -70,6 +74,8 @@ export class CustomHammerConfig extends HammerGestureConfig {
     AuthService,
     MemberDetailResolver,
     MemberListResolver,
+    MemberEditResolver,
+    PreventUnsavedChanges,
     { provide: HAMMER_GESTURE_CONFIG, useClass: CustomHammerConfig }
   ],
   bootstrap: [AppComponent]
