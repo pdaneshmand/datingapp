f7d80d77808029fc4969ff453387c206c5c6c320
Pourdad Daneshmand <pourdad.daneshmand@gmail.com>
2020-02-27 14:44:14 +0330

    signup

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -3,10 +3,15 @@ import {
   HAMMER_GESTURE_CONFIG,
   HammerGestureConfig
 } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-import { FormsModule } from "@angular/forms";
-import { BsDropdownModule, TabsModule } from "ngx-bootstrap";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import {
+  BsDropdownModule,
+  TabsModule,
+  BsDatepickerModule
+} from "ngx-bootstrap";
 import { RouterModule } from "@angular/router";
 import { JwtModule } from "@auth0/angular-jwt";
 import { NgxGalleryModule } from "ngx-gallery";
@@ -58,10 +63,13 @@ export class CustomHammerConfig extends HammerGestureConfig {
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     TabsModule.forRoot(),
     BsDropdownModule.forRoot(),
+    BsDatepickerModule.forRoot(),
     RouterModule.forRoot(appRoutes),
     NgxGalleryModule,
     FileUploadModule,
--- DatingApp-SPA/src/app/members/member-list/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/member-list/photo-editor/photo-editor.component.ts
@@ -63,6 +63,9 @@ export class PhotoEditorComponent implements OnInit {
           isMain: res.isMain
         };
         this.photos.push(photo);
+        if (photo.isMain) {
+          this.authService.changeMemberPhoto(photo.url);
+        }
       }
     };
   }
@@ -75,7 +78,6 @@ export class PhotoEditorComponent implements OnInit {
           this.currentMain = this.photos.filter(p => p.isMain === true)[0];
           this.currentMain.isMain = false;
           photo.isMain = true;
-          this.authService.changeMemberPhoto(photo.url);
           // this.authService.currentUser.photoUrl = photo.url;
           // localStorage.setItem(
           //   "user",
