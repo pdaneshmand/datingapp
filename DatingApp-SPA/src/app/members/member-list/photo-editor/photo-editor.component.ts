be41887474ccbaeaca8fb350b624ba5cc1dc0faa
Pourdad Daneshmand <pourdad.daneshmand@gmail.com>
2020-02-28 16:27:50 +0330

    add paging to api and front

--- DatingApp-SPA/src/app/members/member-list/photo-editor/photo-editor.component.ts
+++ DatingApp-SPA/src/app/members/member-list/photo-editor/photo-editor.component.ts
@@ -78,6 +78,7 @@ export class PhotoEditorComponent implements OnInit {
           this.currentMain = this.photos.filter(p => p.isMain === true)[0];
           this.currentMain.isMain = false;
           photo.isMain = true;
+          this.authService.changeMemberPhoto(photo.url);
           // this.authService.currentUser.photoUrl = photo.url;
           // localStorage.setItem(
           //   "user",
