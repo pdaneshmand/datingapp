b87f199db0a9a51aa26b925b72d1e7a0c0a93f75
Pourdad Daneshmand <pourdad.daneshmand@gmail.com>
2020-03-01 19:37:21 +0330

    Finish Last Session , Messaging Module

--- DatingApp-SPA/src/app/members/member-list/member-detail/member-detail.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-detail/member-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild } from "@angular/core";
 import { User } from "src/app/_models/user";
 import { UserService } from "src/app/_services/user.service";
 import { AlertifyService } from "src/app/_services/alertify.service";
@@ -9,6 +9,7 @@ import {
   NgxGalleryAnimation
 } from "ngx-gallery";
 import { AuthService } from "src/app/_services/auth.service";
+import { TabsetComponent } from "ngx-bootstrap";
 
 @Component({
   selector: "app-member-detail",
@@ -16,9 +17,11 @@ import { AuthService } from "src/app/_services/auth.service";
   styleUrls: ["./member-detail.component.css"]
 })
 export class MemberDetailComponent implements OnInit {
+  @ViewChild("memberTabs", { static: true }) memberTabs: TabsetComponent;
   user: User;
   galleryOptions: NgxGalleryOptions[];
   galleryImages: NgxGalleryImage[];
+
   constructor(
     private authService: AuthService,
     private userService: UserService,
@@ -29,6 +32,11 @@ export class MemberDetailComponent implements OnInit {
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.user = data["user"];
+    });
+
+    this.route.queryParams.subscribe(params => {
+      const selectedTab = params["tab"];
+      this.memberTabs.tabs[selectedTab > 0 ? selectedTab : 0].active = true;
     });
 
     this.galleryOptions = [
@@ -68,5 +76,9 @@ export class MemberDetailComponent implements OnInit {
           this.alertify.error(error);
         }
       );
+  }
+
+  selectTab(tabId: number) {
+    this.memberTabs.tabs[tabId].active = true;
   }
 }
--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -116,5 +116,51 @@ namespace DatingApp.API.Data
         {
             return await _context.SaveChangesAsync() > 0;
         }
+
+        public async Task<Message> GetMessage(int id)
+        {
+            return await _context.Messages.FirstOrDefaultAsync(m => m.Id == id);
+        }
+
+        public async Task<PagedList<Message>> GetMessagesForUser(MessageParam messageParam)
+        {
+            var messages = _context.Messages.Include(u => u.Sender).ThenInclude(p => p.Photos)
+            .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+            .AsQueryable();
+
+            switch (messageParam.MessageContainer)
+            {
+                case "Inbox":
+                    messages = messages.Where(u => u.RecipientId == messageParam.UserId && u.RecipientDeleted == false);
+                    break;
+                case "Outbox":
+                    messages = messages.Where(u => u.SenderId == messageParam.UserId && u.SenderDeleted == false);
+                    break;
+                default:
+                    messages = messages.Where(u => u.RecipientId == messageParam.UserId && u.RecipientDeleted == false && u.IsRead == false);
+                    break;
+            }
+
+            messages = messages.OrderByDescending(d => d.MessageSent);
+
+            return await PagedList<Message>.CreateAsync(messages,
+             messageParam.PageNumber, messageParam.PageSize);
+        }
+
+        public async Task<IEnumerable<Message>> GetMessageThread(int userId, int recipientId)
+        {
+            var messages = await _context.Messages.Include(u => u.Sender).ThenInclude(p => p.Photos)
+            .Include(u => u.Recipient).ThenInclude(p => p.Photos)
+            .Where(m => m.RecipientId == userId
+                && m.RecipientDeleted == false
+                && m.SenderId == recipientId
+                || m.RecipientId == recipientId
+                && m.SenderId == userId
+                && m.SenderDeleted == false).OrderByDescending(m => m.MessageSent)
+            .ToListAsync();
+
+            return messages;
+
+        }
     }
 }
